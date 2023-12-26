8f5057f9fa701932255995cdfb2fa7e185d8056f
chandra11148 <cshekhar11148@gmail.com>
2023-12-26 16:29:16 +0530

    Carousel

---
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,41 @@
+// import Swiper core and required modules
+import { Navigation} from "swiper";
+import React,{useEffect} from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import styles from "./Carousel.module.css";
+import CarouselLeftNavigation from "./CarouselLeftNavigation/CarouselLeftNavigation";
+import CarouselRightNavigation from "./CarouselRightNavigation/CarouselRightNavigation";
+
+const Controls =({data})=>{
+  const swiper = useSwiper();
+  useEffect(()=>{
+    swiper.slideTo(0);
+  },[data]);
+  return <></>;
+
+}
+
+function Carousel({data,renderComponent}) {
+  return (
+    <div className={styles.wraper}>
+      <Swiper
+        // install Swiper modules
+
+        initialSlide={0}
+        modules={[Navigation]}
+        spaceBetween={40}
+        slidesPerView={"auto"}
+        allowTouchMove
+      >
+        <Controls data={data}/>
+        <CarouselLeftNavigation/>
+        <CarouselRightNavigation/>
+        {data.map((el)=><SwiperSlide>{renderComponent(el)}</SwiperSlide>)}
+      </Swiper>
+    </div>
+  );
+}
+export default Carousel;
